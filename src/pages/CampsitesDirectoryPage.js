55c57698e99e83ae111ebfe24f2b604df6a73afa
Bridget Laurenson <balaurenson@gmail.com>
2022-08-27 15:01:41 -0500

    React Week 2: useState

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,30 +1,24 @@
-import { Container, Row, Col, Button } from 'reactstrap';
+import { useState } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import CampsitesDetail from '../features/campsites/CampsitesDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
-
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    };
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
 
     return (
         <Container>
-            <Button onClick={() => toggleCampsite()}>
-                Select Random Campsite
-            </Button>
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsitesList />
-                </Col>
-                <Col sm='7' md='5'>
-                    <CampsitesDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
-        </Container>
+        <Row>
+            <Col sm='5' md='7'>
+                <CampsitesList setCampsiteId={setCampsiteId}/>
+            </Col>
+            <Col sm='7' md='5'>
+                <CampsitesDetail campsite={selectedCampsite} />
+            </Col>
+        </Row>
+    </Container>
     );
 };
 
