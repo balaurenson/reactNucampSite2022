9e85d67805d62651503611eb4dd6cfa6d1974fdb
Bridget Laurenson <balaurenson@gmail.com>
2022-08-31 22:08:18 -0500

    React Week 3: ContactPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,71 +1,51 @@
-import { Col, Row, Container, Card, CardBody, CardHeader } from 'reactstrap';
-import PartnersList from '../features/partners/PartnersList';
+
+import { Container, Col, Row } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
 
-const AboutPage = () => {
+const ContactPage = () => {
     return (
         <Container>
-            <SubHeader current='About Us' />
-            <Row classname='row-content'>
-                <Col sm='6'>
-                    <h3>Our Mission</h3>
-                    <p>
-                        We present a curated database of the best campsites in the
-                        vast woods and backcountry of the World Wide Web Wilderness.
-                        We increase access to adventure for the public while
-                        promoting safe and respectful use of resources. The expert
-                        wilderness trekkers on our staff personally verify each
-                        campsite to make sure that they are up to our standards. We
-                        also present a platform for campers to share reviews on
-                        campsites they have visited with each other.
-                    </p>
-                </Col>
-                <Col sm='6'>
-                    <Card>
-                        <CardHeader className='bg-primary text-white'>
-                            <h3>Facts at a Glance</h3>
-                        </CardHeader>
-                        <CardBody>
-                            <dl className='row'>
-                                <dt className='col-6'>Founded</dt>
-                                <dd className='col-6'>February 3, 2016</dd>
-                                <dt className='col-6'>No. of Campsites in 2019</dt>
-                                <dd className='col-6'>563</dd>
-                                <dt className='col-6'>No. of Reviews in 2019</dt>
-                                <dd className='col-6'>4388</dd>
-                                <dt className='col-6'>Employees</dt>
-                                <dd className='col-6'>42</dd>
-                            </dl>
-                        </CardBody>
-                    </Card>
+            <SubHeader current='Contact Us' />
+
+            <Row className='row-content align-items-center'>
+                <Col sm='4'>
+                    <h5>Our Address</h5>
+                    <address>
+                        1 Nucamp Way
+                        <br />
+                        Seattle, WA 98001
+                        <br />
+                        U.S.A.
+                    </address>
                 </Col>
                 <Col>
-                    <Card className='bg-light mt-3'>
-                        <CardBody className='blockquote'>
-                            <p>
-                                I will not follow where the path may lead, but I
-                                will go where there is no path, and I will leave
-                                a trail.
-                            </p>
-                            <footer className='blockquote-footer'>
-                                Muriel Strode,{' '}
-                                <cite title='Source Title'>
-                                    "Wind-Wafted Wild Flowers" - The Open Court,
-                                    1903
-                                </cite>
-                            </footer>
-                        </CardBody>
-                    </Card>
+                    <a
+                        role='button'
+                        className='btn btn-link'
+                        href='tel:+12065551234'
+                    >
+                        <i className='fa fa-phone' /> 1-206-555-1234
+                    </a>
+                    <br />
+                    <a
+                        role='button'
+                        className='btn btn-link'
+                        href='mailto:fakeemail@fakeemail.co'
+                    >
+                        <i className='fa fa-envelope-o' /> campsites@nucamp.co
+                    </a>
                 </Col>
             </Row>
-            <Row classname='row-content'>
+
+            <Row className='row-content'>
                 <Col xs='12'>
-                    <h3>Community Partners</h3>
+                    <h2>Send Us Your Feedback</h2>
+                    <hr />
                 </Col>
-                <PartnersList />
+                <Col md='10'>TBD: ContactForm</Col>
             </Row>
         </Container>
-    )
+    );
 };
 
-export default AboutPage;
+export default ContactPage;
